34682585c030c0db52d8946da3d2d420fc210f1a
nahiantechx <nahian.easytechx@gmail.com>
2025-09-18 17:51:23 +0600

    updated icons

--- src/components/CounterPart.jsx
+++ src/components/CounterPart.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import cities from "../assets/citiesicon.png";
 import residencies from "../assets/residencies.png";
-import beds from "../assets/beds.png";
 import CountUp from 'react-countup';
+import { FaRegBuilding } from "react-icons/fa";
+import { FaBed } from "react-icons/fa";
 const CounterPart = () => {
   return (
-    <div className="container mx-auto py-8">
-      <div className="flex ">
+    <div className="w-full mx-auto py-8">
+      <div className={`flex bg-gradient-to-r from-[#1e2c08] to-[#80bf1e] text-white`}>
         <div className="w-1/3 shadow-xl flex flex-wrap lg:space-x-2  justify-center rounded-xl lg:shadow-none lg:rounded-none">
-      <img src={cities} alt="citiesicon.png"  className="mr-2"/>
+    <FaRegBuilding className="mt-2 text-3xl"/>
           <p className="text-2xl lg:text-[30px]  text-center lg:text-xl mt-2">
             <CountUp end={15} /> + Residences
           </p>
         </div>
         <div className="w-1/3 shadow-xl flex flex-wrap lg:space-x-2  justify-center rounded-xl lg:shadow-none lg:rounded-none">
-      <img src={residencies} alt="citiesicon.png"  className="mr-2"/>
+    <FaRegBuilding className="mt-2 text-3xl"/>
           <p className="text-2xl lg:text-[30px]  text-center lg:text-xl mt-2">
             <CountUp end={450} /> + Residences
           </p>
         </div>
         <div className="w-1/3 shadow-xl flex flex-wrap lg:space-x-2  justify-center rounded-xl lg:shadow-none lg:rounded-none">
-      <img src={cities} alt="citiesicon.png" className="mr-2" />
+     <FaBed className="mt-1 lg:mt-2 text-[45.5px]"/>
           <p className="text-2xl lg:text-[30px] text-center lg:text-xl mt-2">
             <CountUp end={50000} /> + Beds
           </p>
